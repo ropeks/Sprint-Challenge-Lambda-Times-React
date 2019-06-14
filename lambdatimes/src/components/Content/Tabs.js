0db322114e469c46e8df29d5c6adffef002c1220
ropeks <ropeksmilioner@gmail.com>
2019-06-14 10:35:25 +0200

    show tabs and cards on the screen

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Tab from './Tab';
+import uuid from 'uuid';
+
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        <span className="title">TRENDING TOPICS: <br/></span>
+        {/* 
+            map over the tabs provided on your props, create a new Tab component for each one.
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props
+          */
+          props.tabs.map(tab => (
+            <Tab 
+              key={uuid()} 
+              tab={tab} 
+              selectedTab={props.selectedTab} 
+              selectTabHandler={props.selectTabHandler} 
+            />
+          ))
+        }
       </div>
     </div>
   );
